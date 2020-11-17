8f9acfe01b17787e12864c52cfe1cb9b5d4f9a81
Luthaf <luthaf@luthaf.fr>
2020-11-17 16:30:19 +0100

    Update documentation

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -2,14 +2,17 @@ import sphinx_bootstrap_theme
 import os
 import sys
 
+sys.path.insert(0, os.path.join(os.path.abspath(os.path.join("..", "..")), "python"))
+import chemiscope  # noqa
+
 # -- Project information -----------------------------------------------------
 
-project = 'chemiscope'
-copyright = '2020, Guillaume Fraux'
-author = 'Guillaume Fraux'
+project = "chemiscope"
+copyright = "2020, Guillaume Fraux"
+author = "Guillaume Fraux"
 
 # The full version, including alpha/beta/rc tags
-release = '0.0'
+release = chemiscope.__version__
 
 # -- General configuration ---------------------------------------------------
 
@@ -17,44 +20,40 @@ release = '0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
+    "sphinx.ext.autodoc",
 ]
 
-sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", "utils"))
-
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 
-html_theme = 'bootstrap'
+html_theme = "bootstrap"
 html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 
 html_theme_options = {
-    'navbar_links': [
+    "navbar_links": [
         ("Documentation", "index.html", True),
     ],
-    'navbar_pagenav': False,
-    'bootswatch_theme': "flatly",
-    'source_link_position': None,
+    "navbar_pagenav": False,
+    "bootswatch_theme": "flatly",
+    "source_link_position": None,
 }
 
-html_sidebars = {
-    '**': ['sidebar-toc.html', 'searchbox.html']
-}
+html_sidebars = {"**": ["sidebar-toc.html", "searchbox.html"]}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = [os.path.join('..', '_static')]
+html_static_path = [os.path.join("..", "_static")]
 
-templates_path = [os.path.join('..', '_templates')]
+templates_path = [os.path.join("..", "_templates")]
 
 
 def setup(app):
