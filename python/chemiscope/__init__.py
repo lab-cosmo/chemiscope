ca72275d0313a5deb057cb635449177756c614e6
Luthaf <luthaf@luthaf.fr>
2020-11-18 11:27:26 +0100

    Centralize the version number to package.json
    Read the json file to access it in setup.py, and use setuptools
    to set chemiscope.__version__ accordingly

--- python/chemiscope/__init__.py
+++ python/chemiscope/__init__.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 from .input import write_input
 
-# TODO: can we populate all of `chemiscope.__version__`, the version field in
-# package.json and the PyPI version in setup.cfg with a single source?
-__version__ = "0.2.0"
+
+# Get the version from setuptools metadata (which took it from package.json)
+# cf https://stackoverflow.com/a/17638236/4692076
+from pkg_resources import get_distribution, DistributionNotFound
+import os
+
+try:
+    dist = get_distribution("chemiscope")
+    # Normalize case for Windows systems
+    dist_loc = os.path.normcase(dist.location)
+    here = os.path.normcase(__file__)
+    if not here.startswith(os.path.join(dist_loc, "chemiscope")):
+        # not installed, but there is another version that *is*
+        raise DistributionNotFound
+except DistributionNotFound:
+    __version__ = "dev"
+else:
+    __version__ = dist.version
