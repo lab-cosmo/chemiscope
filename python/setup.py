265190e7a2c13df4f270be6b59df6888fbb4515c
Luthaf <luthaf@luthaf.fr>
2020-11-19 11:35:12 +0100

    Fix installation of python package from a sdist

--- python/setup.py
+++ python/setup.py
@@ -5,6 +5,6 @@ import os
 
 if __name__ == "__main__":
     # get the version directly from the package.json file
-    package_json = os.path.join(os.path.dirname(__file__), "..", "package.json")
+    package_json = os.path.join(os.path.dirname(__file__), "package.json")
     version = json.load(open(package_json))["version"]
     setup(version=version)
