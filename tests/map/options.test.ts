4458150e004978151b01a860d127fa71af697dc7
Jakub Lála <jakublala@gmail.com>
2021-06-30 11:52:26 +0200

    Added a unit test

--- tests/map/options.test.ts
+++ tests/map/options.test.ts
@@ -1,4 +1,5 @@
-import { MapOptions } from '../../src/map/options';
+import { MapOptions, AxisOptions } from '../../src/map/options';
+import { getByID } from '../../src/utils';
 
 import { default as setupJSDOM } from '../jsdom';
 import { assert } from 'chai';
@@ -29,5 +30,29 @@ describe('MapOptions', () => {
         options.remove();
         assert(document.body.innerHTML === '');
         assert(root.innerHTML === '');
+    });
+
+    it('scale label switches between linear and log', () => {
+        const root = document.createElement('div');
+        const options = new MapOptions(root, DUMMY_PROPERTIES, DUMMY_CALLBACK);
+
+        assertScaleLabel(options.x, 'x');
+        assertScaleLabel(options.y, 'y');
+        assertScaleLabel(options.z, 'z');
+
+        function assertScaleLabel(axisOptions: AxisOptions, axisName: string): void {
+            const min = getByID(`chsp-${axisName}-min-label`);
+            const max = getByID(`chsp-${axisName}-max-label`);
+
+            // change from linear (default) to log scale
+            options.setLogLabel(axisOptions, axisName);
+            assert(min.innerHTML === 'min: 10^');
+            assert(max.innerHTML === 'max: 10^');
+
+            // change back from log to linear
+            options.setLogLabel(axisOptions, axisName);
+            assert(min.innerHTML === 'min:');
+            assert(max.innerHTML === 'max:');
+        }
     });
 });
