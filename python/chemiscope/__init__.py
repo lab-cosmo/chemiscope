0c0c1dcdc77aee069265cc5d26ec7444de7ed903
Luthaf <luthaf@luthaf.fr>
2021-08-31 13:35:03 +0200

    Improve Python side of the jupyter widget
    We can now save the data to a file directly

--- python/chemiscope/__init__.py
+++ python/chemiscope/__init__.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 from .input import create_input, write_input  # noqa
-from .widget import display  # noqa
+
+try:
+    # only import the chemiscope.show function if we have ipywidgets installed.
+    import ipywidgets  # noqa
+    from .jupyter import show  # noqa
+except ImportError:
+    pass
 
 __version__ = "0.3.0"
