23a7110c9b65bbd7a24b951b720d6deaf862cc60
Michele Ceriotti <ceriottm@users.noreply.github.com>
2023-10-16 10:07:43 -0700

    Allow input of list of lists in the properties for python bindings (#297)

--- python/chemiscope/input.py
+++ python/chemiscope/input.py
@@ -702,31 +702,34 @@ def _linearize(name, property, n_structures, n_centers, multidimensional=False):
     _validate_property(name, property)
 
     data = {}
+    values = property["values"]
     if not multidimensional:
-        if isinstance(property["values"], list):
+        if isinstance(values, list) and isinstance(values[0], list):
+            # convert to ndarray so we can use the parser below
+            values = np.array(values)
+        if isinstance(values, list):
+            # deal with simple list
             data[name] = {
                 "target": property["target"],
-                "values": _typetransform(property["values"], name),
+                "values": _typetransform(values, name),
             }
-        elif isinstance(property["values"], np.ndarray):
-            if len(property["values"].shape) == 1:
+        elif isinstance(values, np.ndarray):
+            if len(values.shape) == 1:
                 data[name] = {
                     "target": property["target"],
-                    "values": _typetransform(list(property["values"]), name),
+                    "values": _typetransform(list(values), name),
                 }
-            elif len(property["values"].shape) == 2:
-                if property["values"].shape[1] == 1:
+            elif len(values.shape) == 2:
+                if values.shape[1] == 1:
                     data[name] = {
                         "target": property["target"],
-                        "values": _typetransform(list(property["values"]), name),
+                        "values": _typetransform(list(values), name),
                     }
                 else:
-                    for i in range(property["values"].shape[1]):
+                    for i in range(values.shape[1]):
                         data[f"{name}[{i + 1}]"] = {
                             "target": property["target"],
-                            "values": _typetransform(
-                                list(property["values"][:, i]), name
-                            ),
+                            "values": _typetransform(list(values[:, i]), name),
                         }
             else:
                 raise Exception("unsupported ndarray property")
@@ -737,12 +740,12 @@ def _linearize(name, property, n_structures, n_centers, multidimensional=False):
     else:
         assert isinstance(property["parameters"][0], str)
 
-        values = []
-        for i in range(len(property["values"])):
-            values += [_typetransform(list(property["values"][i]), name)]
+        ndvalues = []
+        for i in range(len(values)):
+            ndvalues += [_typetransform(list(values[i]), name)]
         data[name] = {
             "target": property["target"],
-            "values": values,
+            "values": ndvalues,
             "parameters": property["parameters"],
         }
 
