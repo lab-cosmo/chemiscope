f47dc9bf9361d6cfa37d70fc08594273ca409f8c
Sofiia Chorna <chernaya.sofiia@gmail.com>
2024-05-22 17:45:30 +0200

    replace font-awesome icon with svg + move styles to css file

--- python/chemiscope/sphinx_gallery.py
+++ python/chemiscope/sphinx_gallery.py
@@ -5,7 +5,7 @@ from sphinx_gallery.scrapers import matplotlib_scraper
 
 class ChemiscopeScraper:
     def __repr__(self):
-        return 'ChemiscopeScraper'
+        return "ChemiscopeScraper"
 
     def __call__(self, block, block_vars, gallery_conf):
         variables = block_vars.get("example_globals", {})
@@ -13,7 +13,7 @@ class ChemiscopeScraper:
         if widget:
             # Save the dataset to json
             path_png = block_vars.get("image_path_iterator").next()
-            path_json = path_png.replace('.png', '.json.gz')
+            path_json = path_png.replace(".png", ".json.gz")
             widget.save(path_json)
 
             # Use the custom html content
@@ -37,40 +37,28 @@ class ChemiscopeScraper:
             <script src="../../../_static/js/chemiscope.min.js"></script>
             <script src="../../../_static/js/chemischope-sphinx-gallery.js"></script>
 
-            <!-- Font-awesome -->
-            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" />
+            <!-- CSS styles -->
+            <link rel="stylesheet" href="../../../_static/css/chemischope-sphinx-gallery.css" type="text/css" />
             <link rel="icon" type="image/png" href="../../../_static/chemiscope-icon.png" sizes="32x32" />
 
-            <!-- CSS styles -->
-            <link rel="stylesheet" href="../../../_static/chemischope-sphinx-gallery.css" type="text/css" />
-
             <!-- Warning Display -->
-            <div id="{div_id}-warning-display"
-                style="display: none;
-                flex-wrap: wrap;
-                border: 1px solid #f0ad4e;
-                background-color: #fcf8e3;
-                color: #8a6d3b;
-                padding: 15px;
-                border-radius: 4px;
-                margin-bottom: 20px;">
-                    <p style="flex: 1"></p>
-                    <button
-                        type="button"
-                        style="position: relative; float: right; font-size: 3em; line-height: 1; color: inherit; text-shadow: none; background-color: transparent; border: 0; -webkit-appearance: none;"
-                        aria-label="Close"
-                        onclick="hideElement('{div_id}-warning-display')"
-                    >&times;</button>`
+            <div id="{div_id}-warning-display" class="chemiscope-sphinx-gallery-warning">
+                <p></p>
+                <button
+                    type="button"
+                    aria-label="Close"
+                    onclick="hideElement('{div_id}-warning-display')"
+                >&times;</button>
             </div>
-            <div style="padding: 0.5rem 1.25rem">&nbsp;</div>
+            <div style="padding: 0.5rem 1.25rem" />
 
             <!-- Loading Spinner -->
-            <div id="{div_id}-loading">
-                <i class="fa fa-spinner fa-spin"></i>
+            <div id="{div_id}-loading" class="chemiscope-sphinx-gallery-spinner">
+                <img src="../../../_static/loading-icon.svg" alt="Loading icon" />
             </div>
 
             <!-- Chemiscope Visualization -->
-            <div id="{div_id}"></div>
+            <div id="{div_id}" />
 
             <!-- Load Visualization Script -->
             <script>
@@ -99,7 +87,7 @@ def copy_additional_files(app, exception):
 def copy_files(src_dir, build_dir):
     for root, _, files in os.walk(src_dir):
         for file in files:
-            if file.endswith('.json.gz'):
+            if file.endswith(".json.gz"):
                 src_file = os.path.join(root, file)
                 dst_file = os.path.join(build_dir, file)
                 copy_file(src_file, dst_file)
@@ -108,4 +96,3 @@ def copy_file(src_file, dst_file):
     dst_dir = os.path.dirname(dst_file)
     os.makedirs(dst_dir, exist_ok=True)
     shutil.copyfile(src_file, dst_file)
-    print(f"Copied {src_file} to {dst_file}")
