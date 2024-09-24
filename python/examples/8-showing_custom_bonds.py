cb1ef9154c6999f3122d9e3175c1c5bef2e20c2d
andrewtarzia <andrew.tarzia@gmail.com>
2024-09-24 17:26:00 +0200

    Update example function.

--- python/examples/8-showing_custom_bonds.py
+++ python/examples/8-showing_custom_bonds.py
@@ -1,173 +1,180 @@
 """
-Showing custom bonds
-====================
+Showing custom bonds using stk
+==============================
 
 This example demonstrates how to add shapes into the chemiscope output such
 that custom bonds that would not normally be assigned by ASE can be rendered.
 
+This is done by using stk to generate and analyse molecules, which comes with
+topology/bonding information by default (using the cheminformatic software
+rdkit).
+
 """
 
-# %%
-#
+import pathlib
+
 import stk
 import stko
-import ase.io
-import pathlib
+from rdkit.Chem import AllChem as rdkit
+
 import chemiscope
 
 working_path = pathlib.Path(__file__).resolve().parent
 data_path = working_path / "data"
 
-########################################################################
-# This is just for development, where I build the library of structures.
+# %%
+#
+# Generate a list of stk BuildingBlocks (representation of a molecule) with
+# properties. This also includes working with rdkit, which comes installed
+# with stk.
+
+rdkitmol = rdkit.MolFromSmiles("Cc1ccccc1")
+rdkitmol = rdkit.AddHs(rdkitmol)
+rdkit.Kekulize(rdkitmol)
+params = rdkit.ETKDGv3()
+params.randomSeed = 0xF00D
+rdkit.EmbedMolecule(rdkitmol, params)
+
+structures = [
+    # A building block.
+    stk.BuildingBlock(smiles="NCCN"),
+    # A mostly optimised cage molecule.
+    stk.ConstructedMolecule(
+        topology_graph=stk.cage.FourPlusSix(
+            building_blocks=(
+                stk.BuildingBlock(
+                    smiles="NCCN",
+                    functional_groups=[stk.PrimaryAminoFactory()],
+                ),
+                stk.BuildingBlock(
+                    smiles="O=CC(C=O)C=O",
+                    functional_groups=[stk.AldehydeFactory()],
+                ),
+            ),
+            optimizer=stk.MCHammer(),
+        ),
+    ),
+    # A metal-organic cage.
+    stk.ConstructedMolecule(
+        stk.cage.M2L4Lantern(
+            building_blocks=(
+                stk.BuildingBlock(
+                    smiles="[Pd+2]",
+                    functional_groups=(
+                        stk.SingleAtom(stk.Pd(0, charge=2)) for i in range(4)
+                    ),
+                    position_matrix=[[0.0, 0.0, 0.0]],
+                ),
+                stk.BuildingBlock(
+                    smiles=("C1=NC=CC(C2=CC=CC(C3=C" "C=NC=C3)=C2)=C1"),
+                    functional_groups=[
+                        stk.SmartsFunctionalGroupFactory(
+                            smarts="[#6]~[#7X2]~[#6]",
+                            bonders=(1,),
+                            deleters=(),
+                        ),
+                    ],
+                ),
+            ),
+            # Ensure that bonds between the
+            # GenericFunctionalGroups of the ligand and the
+            # SingleAtom functional groups of the metal are
+            # dative.
+            reaction_factory=stk.DativeReactionFactory(
+                stk.GenericReactionFactory(
+                    bond_orders={
+                        frozenset(
+                            {
+                                stk.GenericFunctionalGroup,
+                                stk.SingleAtom,
+                            }
+                        ): 9,
+                    },
+                ),
+            ),
+        ),
+    ),
+    # A host guest molecule.
+    stk.ConstructedMolecule(
+        topology_graph=stk.host_guest.Complex(
+            host=stk.BuildingBlock.init_from_molecule(
+                stk.ConstructedMolecule(
+                    topology_graph=stk.cage.FourPlusSix(
+                        building_blocks=(
+                            stk.BuildingBlock(
+                                smiles="NC1CCCCC1N",
+                                functional_groups=[
+                                    stk.PrimaryAminoFactory(),
+                                ],
+                            ),
+                            stk.BuildingBlock(
+                                smiles="O=Cc1cc(C=O)cc(C=O)c1",
+                                functional_groups=[stk.AldehydeFactory()],
+                            ),
+                        ),
+                        optimizer=stk.MCHammer(),
+                    ),
+                )
+            ),
+            guests=stk.host_guest.Guest(
+                building_block=stk.BuildingBlock("[Br][Br]"),
+            ),
+        ),
+    ),
+    # From rdkit.
+    stk.BuildingBlock.init_from_rdkit_mol(rdkitmol),
+]
+
+# Write to file for sanity checks.
+for i, struct in enumerate(structures):
+    struct.write(data_path / f"stk_{i}.mol")
+
+# %%
+#
+# Write their properties using any method, here we show using stko:
+# https://stko-docs.readthedocs.io/en/latest/
+
 energy = stko.UFFEnergy()
 analyser = stko.molecule_analysis.GeometryAnalyser()
-xyz_path = data_path / "stkbuilt.xyz"
-string = ""
-
-host = stk.ConstructedMolecule(
-    topology_graph=stk.cage.FourPlusSix(
-        building_blocks=(
-            stk.BuildingBlock(
-                smiles="NC1CCCCC1N",
-                functional_groups=[
-                    stk.PrimaryAminoFactory(),
-                ],
-            ),
-            stk.BuildingBlock(
-                smiles="O=Cc1cc(C=O)cc(C=O)c1",
-                functional_groups=[stk.AldehydeFactory()],
-            ),
-        ),
-        optimizer=stk.MCHammer(),
-    ),
-)
-molecule = stk.ConstructedMolecule(
-    topology_graph=stk.host_guest.Complex(
-        host=stk.BuildingBlock.init_from_molecule(host),
-        guests=stk.host_guest.Guest(
-            building_block=stk.BuildingBlock("[Br][Br]"),
-        ),
-    ),
-)
-teststring = stk.XyzWriter().to_string(molecule)
-teststring = teststring.split("\n")
-teststring[1] = (
-    f"e='{energy.get_energy(molecule)}' "
-    f"rg='{analyser.get_radius_gyration(molecule)}'"
-)
-string += "\n".join(teststring)
-
-bb1 = stk.BuildingBlock(
-    smiles="NCCN",
-    functional_groups=[stk.PrimaryAminoFactory()],
-)
-bb2 = stk.BuildingBlock(
-    smiles="O=CC(C=O)C=O",
-    functional_groups=[stk.AldehydeFactory()],
-)
-molecule = stk.ConstructedMolecule(
-    topology_graph=stk.cage.FourPlusSix((bb1, bb2)),
-)
-molecule.write(data_path / "unoptcage.mol")
-teststring = stk.XyzWriter().to_string(molecule)
-teststring = teststring.split("\n")
-teststring[1] = (
-    f"e='{energy.get_energy(molecule)}' "
-    f"rg='{analyser.get_radius_gyration(molecule)}'"
-)
-string += "\n".join(teststring)
-
-molecule = stk.ConstructedMolecule(
-    topology_graph=stk.cage.FourPlusSix(
-        building_blocks=(bb1, bb2),
-        optimizer=stk.MCHammer(),
-    )
-)
-teststring = stk.XyzWriter().to_string(molecule)
-teststring = teststring.split("\n")
-teststring[1] = (
-    f"e='{energy.get_energy(molecule)}' "
-    f"rg='{analyser.get_radius_gyration(molecule)}'"
-)
-string += "\n".join(teststring)
-
-
-palladium_atom = stk.BuildingBlock(
-    smiles="[Pd+2]",
-    functional_groups=(stk.SingleAtom(stk.Pd(0, charge=2)) for i in range(4)),
-    position_matrix=[[0.0, 0.0, 0.0]],
-)
-bb1 = stk.BuildingBlock(
-    smiles=("C1=NC=CC(C2=CC=CC(C3=C" "C=NC=C3)=C2)=C1"),
-    functional_groups=[
-        stk.SmartsFunctionalGroupFactory(
-            smarts="[#6]~[#7X2]~[#6]",
-            bonders=(1,),
-            deleters=(),
-        ),
-    ],
-)
-molecule = stk.ConstructedMolecule(
-    stk.cage.M2L4Lantern(
-        building_blocks=(palladium_atom, bb1),
-        # Ensure that bonds between the
-        # GenericFunctionalGroups of the ligand and the
-        # SingleAtom functional groups of the metal are
-        # dative.
-        reaction_factory=stk.DativeReactionFactory(
-            stk.GenericReactionFactory(
-                bond_orders={
-                    frozenset(
-                        {
-                            stk.GenericFunctionalGroup,
-                            stk.SingleAtom,
-                        }
-                    ): 9,
-                },
-            ),
-        ),
-    ),
-)
-molecule.write(data_path / "metalcage.mol")
-teststring = stk.XyzWriter().to_string(molecule)
-teststring = teststring.split("\n")
-teststring[1] = (
-    f"e='{energy.get_energy(molecule)}' "
-    f"rg='{analyser.get_radius_gyration(molecule)}'"
-)
-string += "\n".join(teststring)
-
-
-with xyz_path.open("w") as f:
-    f.write(string)
-
-########################################################################
-
-
-print(
-    "one change I am interested in, is if you want to use the standard "
-    "load xyz > chemiscope workflow, like here. "
-    "or, if I use rdkit/stk to provide the graph automatically? "
-    "I would not want to add any dependancies in doing so"
-)
+properties = {
+    "e": [energy.get_energy(molecule) for molecule in structures],
+    "rg": [analyser.get_radius_gyration(molecule) for molecule in structures],
+}
 
 
 # %%
 #
-# Load structures from an extended xyz file.
-frames = ase.io.read(xyz_path, ":")
+# Get the stk bonding information and convert them into shapes.
+shape_dict = chemiscope.convert_stk_bonds_as_shapes(
+    frames=structures,
+    bond_color="#fc5500",
+    bond_radius=0.12,
+)
+
+# Write the shape string for settings to turn them on automatically.
+shape_string = ",".join(shape_dict.keys())
 
 
 # %%
 #
-# Write to json file without added shapes and note that the XX-XX bonds are
-# missing.
+# A chemiscope widget can be used to visualize structures and properties.
+# This generates a Chemiscope object that is rendered to an interactive
+# widget when executed in a Jupyter notebook.
+
+chemiscope.show(frames=structures, properties=properties)
+
+
+# %%
+#
+# For sharing with collaborators, or when one does not want to use an interactive
+# notebook, one can also write a JSON (or compressed JSON) file that contains all
+# information about structures and properties, and can be viewed at chemiscope.org
+# Save as a file that can be viewed at chemiscope.org
 
 chemiscope.write_input(
-    "noshape_example.json.gz",
-    frames=frames,
-    properties=chemiscope.extract_properties(frames, only=["e", "rg"]),
+    path="noshape_example.json.gz",
+    frames=structures,
+    properties=properties,
     meta=dict(name="Missing bonds by automation."),
     settings=chemiscope.quick_settings(x="rg", y="e", color=""),
 )
@@ -175,44 +182,22 @@ chemiscope.write_input(
 
 # %%
 #
-# Compute the shape dicionary from the structures based on XX.
-mol1 = stk.BuildingBlock.init_from_file(data_path / "unoptcage.mol")
-mol1_bonds = tuple(
-    (bond.get_atom1().get_id(), bond.get_atom2().get_id()) for bond in mol1.get_bonds()
-)
+# Now with added bonding information.
 
-mol3 = stk.BuildingBlock.init_from_file(data_path / "metalcage.mol")
-mol3_bonds = tuple(
-    (bond.get_atom1().get_id(), bond.get_atom2().get_id()) for bond in mol3.get_bonds()
+chemiscope.show(
+    frames=structures,
+    properties=properties,
+    shapes=shape_dict,
 )
-bonds_per_frames = {
-    # Here, we do it by hand to fill in some issues.
-    0: ((0, 60), (22, 132), (30, 85), (34, 150), (4, 96), (46, 169)),
-    2: ((3, 48), (11, 56), (19, 72)),
-    # In these two examples, we are using "fake", unoptimised models and show
-    # how to generate these from a mol file.
-    # Although these examples currently require a new dependancy, so I can
-    # alter them as needed?
-    1: mol1_bonds,
-    3: mol3_bonds,
-}
-
-shape_dict = chemiscope.convert_bonds_as_shapes(
-    frames=frames,
-    bonds_per_frames=bonds_per_frames,
-)
-# Write the shape string for settings to turn them on automatically.
-shape_string = ",".join(shape_dict.keys())
 
 # %%
 #
-# Write to json file with added shapes and note that the XX-XX bonds are
-# present.
+# Write to json file with added shapes and note are present.
 chemiscope.write_input(
-    "shape_example.json.gz",
-    frames=frames,
-    properties=chemiscope.extract_properties(frames, only=["e", "rg"]),
-    meta=dict(name="Added some bonds."),
+    path="shape_example.json.gz",
+    frames=structures,
+    properties=properties,
+    meta=dict(name="Added all stk bonds."),
     settings=chemiscope.quick_settings(
         x="rg",
         y="e",
@@ -220,11 +205,12 @@ chemiscope.write_input(
         structure_settings={
             "shape": shape_string,
             "atoms": True,
-            "bonds": True,
+            "bonds": False,
             "spaceFilling": False,
         },
     ),
     shapes=shape_dict,
 )
+
 
 raise SystemExit
