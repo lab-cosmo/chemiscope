b8dce486885f81f419d845fc375633fb257d7064
Qianjun Xu <92628709+GardevoirX@users.noreply.github.com>
2024-11-19 22:56:37 +0100

    Update python/chemiscope/jupyter.py
    Co-authored-by: Guillaume Fraux <luthaf@luthaf.fr>

--- python/chemiscope/jupyter.py
+++ python/chemiscope/jupyter.py
@@ -249,6 +249,7 @@ def show(
                 "values": (
                     list(range(len(frames)))
                     if hasattr(frames, "__len__")
+                    # MDAnalysis `frames` will not be a list, and does not have a `__len__`
                     else list(range(len(frames.trajectory)))
                 ),
             }
