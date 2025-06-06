fac539714e25646cd294e972dc84fa06f691c9d0
Sofiia Chorna <chernaya.sofiia@gmail.com>
2025-06-06 14:16:53 +0200

    Add imports

--- python/chemiscope/__init__.py
+++ python/chemiscope/__init__.py
@@ -1,4 +1,9 @@
-from .explore import explore, metatensor_featurizer, metatomic_featurizer  # noqa: F401
+from .explore import (
+    explore,
+    metatensor_featurizer,
+    metatomic_featurizer,
+    sketchmap_featurizer,
+)  # noqa: F401
 from .input import create_input, quick_settings, write_input  # noqa: F401
 from .jupyter import show, show_input  # noqa
 from .structures import (  # noqa: F401
