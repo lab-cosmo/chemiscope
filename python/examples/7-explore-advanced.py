930d7ce36e587fa43ee45bc3bfd712c637ce7ce4
Sofiia Chorna <chernaya.sofiia@gmail.com>
2025-07-14 19:10:37 +0200

    Remove example with soap, merge example with envs

--- python/examples/7-explore-advanced.py
+++ python/examples/7-explore-advanced.py
@@ -116,11 +116,12 @@ chemiscope.show_input("data/mace-off-tsne-qm9.json.gz")
 
 # %%
 #
-# Example with MACE-MP0 and t-SNE
-# +++++++++++++++++++++++++++++++
+# Example with MACE-MP0, t-SNE and environments
+# +++++++++++++++++++++++++++++++++++++++++++++
 #
-# We will define another ``featurize`` function that uses MACE-MP0 to calculate the
-# descriptors and t-SNE for the dimensionality reduction.
+# This example demonstrates how to compute descriptors using the MACE-MP0 and t-SNE with
+# ``environments`` parameter specifying which atoms in the frames are used for
+# calculating the descriptors.
 #
 # Firstly, import mace library.
 
@@ -140,24 +141,43 @@ m3cd_frames = ase.io.read("data/explore_m3cd.xyz", ":")
 
 
 def mace_mp0_tsne(frames, environments):
-    if environments is not None:
-        raise ValueError("'environments' are not supported by this featurizer")
-
     # Initialize a mace-mp0 calculator
     descriptor_opt = {"model": "small", "device": "cpu", "default_dtype": "float64"}
     calculator = mace_mp(**descriptor_opt)
 
     # Calculate the features
-    descriptors = []
-    for frame in frames:
-        structure_avg = np.mean(
-            (calculator.get_descriptors(frame, invariants_only=True)),
-            axis=0,
-        )
-        descriptors.append(structure_avg)
+    if environments is None:
+        descriptors = []
+        for frame in frames:
+            structure_avg = np.mean(
+                (calculator.get_descriptors(frame, invariants_only=True)),
+                axis=0,
+            )
+            descriptors.append(structure_avg)
+    else:
+        grouped_envs = {}
+        unique_structures = set()
+
+        # Group atom indices from environments
+        for structure_index, atom_index, _cutoff in environments:
+            if structure_index not in grouped_envs:
+                grouped_envs[structure_index] = []
+            grouped_envs[structure_index].append(atom_index)
+            unique_structures.add(structure_index)
+
+        # Compute descriptors per specified atom
+        descriptors = []
+        for structure_index in sorted(grouped_envs):
+            atoms = frames[structure_index]
+            atom_indices = grouped_envs[structure_index]
+
+            full_descriptors = calculator.get_descriptors(atoms, invariants_only=True)
+            for atom_index in atom_indices:
+                descriptors.append(full_descriptors[atom_index])
+
     descriptors = np.array(descriptors)
 
-    n_jobs = min(len(frames), os.cpu_count())
+    n_jobs = min(len(descriptors), os.cpu_count())
 
     # Apply t-SNE
     perplexity = min(30, descriptors.shape[0] - 1)
@@ -167,90 +187,10 @@ def mace_mp0_tsne(frames, environments):
 
 # %%
 #
-# Provide the created function to :py:func:`chemiscope.explore`.
-
-cs = chemiscope.explore(m3cd_frames, featurize=mace_mp0_tsne)
-
-# %%
-#
-# To show case the result, we are loading pre-computed data using the ``mace_mp0_tsne``
-# function for 1k structures.
-
-
-fetch_dataset("mace-mp-tsne-m3cd.json.gz")
-chemiscope.show_input("data/mace-mp-tsne-m3cd.json.gz")
-
-
-# %%
-#
-# Example with SOAP, t-SNE and environments
-# +++++++++++++++++++++++++++++++++++++++++
-#
-# This example demonstrates how to compute descriptors using the SOAP and t-SNE with
-# ``environments`` parameter specifying which atoms in the frames are used for
-# calculating the descriptors.
-#
-# We are defining a custom featurizer that takes frames and environments.
-
-from dscribe.descriptors import SOAP  # noqa
-
-
-def soap_tnse_with_environments(frames, environments):
-    if environments is None:
-        raise ValueError("'environments' must be provided")
-
-    grouped_envs = {}
-    unique_structures = set()
-
-    # Get atom-centered indices from environments
-    for [env_index, atom_index, _cutoff] in environments:
-        if env_index not in grouped_envs:
-            grouped_envs[env_index] = []
-        grouped_envs[env_index].append(atom_index)
-        unique_structures.add(env_index)
-    centers = list(grouped_envs.values())
-
-    # only include frames that are present in the environments
-    if len(unique_structures) != len(frames):
-        frames = [frames[index] for index in sorted(unique_structures)]
-
-    # Get global species
-    species = set()
-    for frame in frames:
-        species.update(frame.get_chemical_symbols())
-    species = list(species)
-
-    # Initialize calculator
-    soap = SOAP(
-        species=species,
-        r_cut=4.5,
-        n_max=8,
-        l_max=6,
-        sigma=0.2,
-        rbf="gto",
-        average="outer",
-        periodic=True,
-        weighting={"function": "pow", "c": 1, "m": 5, "d": 1, "r0": 3.5},
-        compression={"mode": "mu1nu1"},
-    )
-
-    # Calculate descriptors
-    feats = soap.create(frames, centers=centers)
-
-    # Compute tsne
-    perplexity = min(30, feats.shape[0] - 1)
-    reducer = TSNE(n_components=2, perplexity=perplexity)
-    return reducer.fit_transform(feats)
-
-
-# %%
-#
 # Provide a created function and environments to :py:func:`chemiscope.explore`. The
 # environments are manually defined following the format ``[index of structure, index
 # of atom, cutoff]``.
 
 chemiscope.explore(
-    frames=m3cd_frames,
-    featurize=soap_tnse_with_environments,
-    environments=[(1, 2, 3.5), (2, 0, 3.5)],
+    m3cd_frames, featurize=mace_mp0_tsne, environments=[(1, 2, 3.5), (2, 0, 3.5)]
 )
