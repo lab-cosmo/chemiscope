46508c440d4a524b350c932363c0cbca1cef8ad8
Michele Ceriotti <ceriottm@users.noreply.github.com>
2025-11-23 07:43:03 +0100

    Update version number to 0.8.7 (#454)
    Title says it all

--- python/examples/1-base.py
+++ python/examples/1-base.py
@@ -3,8 +3,8 @@ Simple chemiscope input
 =======================
 
 This example demonstrates the basic usage of the ``chemiscope`` package, reading
-structures and properties from structures stored as 
- `ASE <https://wiki.fysik.dtu.dk/ase>`_  objects, and
+structures and properties from structures stored as
+`ASE <https://wiki.fysik.dtu.dk/ase>`_  objects, and
 preparing a chemiscope file to visualize them using :py:func:`chemiscope.show`. First,
 import dependencies:
 """
