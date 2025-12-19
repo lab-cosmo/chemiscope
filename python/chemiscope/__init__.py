2f39996a93bdfad313881c126081a528d4007486
Guillaume Fraux <guillaume.fraux@epfl.ch>
2025-12-19 11:59:08 +0100

    Remove `ase_merge_pi_frames`

--- python/chemiscope/__init__.py
+++ python/chemiscope/__init__.py
@@ -10,7 +10,6 @@ from .jupyter import show, show_input  # noqa
 from .structures import (  # noqa: F401
     all_atomic_environments,
     arrow_from_vector,
-    ase_merge_pi_frames,
     ase_tensors_to_ellipsoids,
     ase_vectors_to_arrows,
     ellipsoid_from_tensor,
