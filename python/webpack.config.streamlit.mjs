0f02c674783f0cc8e1a8e3b9c3b36659ecb988a6
Michele Ceriotti <ceriottm@users.noreply.github.com>
2026-01-19 14:08:19 +0100

    Allow faster dev build, especially for jupyter widget development (#491)
    Building takes forever in production mode. This allows setting an environment variable to make it faster when developing

--- python/webpack.config.streamlit.mjs
+++ python/webpack.config.streamlit.mjs
@@ -4,8 +4,11 @@ import path from 'path';
 
 import { WEBPACK_CONFIG } from '../webpack.config.mjs';
 
+const mode = process.env.CHEMISCOPE_WEBPACK_MODE || 'production';
+
 const config = {
     ...WEBPACK_CONFIG,
+    mode: mode,
     entry: './python/streamlit/src/index.ts',
     output: {
         path: path.resolve('./python/streamlit/build'),
